8aca59155787dc5b1ea3cae9a655e68446d4a38c
Ratnakar Singh Parihar <ratnakarsinghparihar9399@gmail.com>
2026-02-01 17:29:24 +0530

    server.js update cors

--- server/server.js
+++ server/server.js
@@ -34,9 +34,9 @@ const { setSocketIO } = require("./controllers/notificationController");
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL, // FRONTEND_URL ko .env me rakho
-    methods: ["GET", "POST"],          // Allowed methods
-    credentials: true,                 // Agar cookies ya auth headers use kar rahe ho
+    origin: process.env.CLIENT_URL ||  "https://jeevandaancare.vercel.app" , 
+    methods: ["GET", "POST"],          
+    credentials: true,                 
   },
 });
 
