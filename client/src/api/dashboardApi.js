64ae565e9f3461ce569ad3a2af2d464e02c01978
Ratnakar Singh Parihar <ratnakarsinghparihar9399@gmail.com>
2026-02-02 05:52:34 +0530

    api update

--- client/src/api/dashboardApi.js
+++ client/src/api/dashboardApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const getDashboardCounts = async () => {
-  const res = await axios.get("http://localhost:5000/api/dashboard/counts");
+  const res = await axios.get(
+    `${import.meta.env.VITE_API_URL}/dashboard/counts`
+  );
   return res.data;
 };
